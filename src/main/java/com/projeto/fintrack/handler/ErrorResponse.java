ea5491ab04f9c7792b84017708ad1ad21e35e089
LucasViana130 <lucasgviana1@gmail.com>
2026-04-06 21:04:16 -0300

    add config

--- src/main/java/com/projeto/fintrack/handler/ErrorResponse.java
+++ src/main/java/com/projeto/fintrack/handler/ErrorResponse.java
@@ -1,4 +1,20 @@
 package com.projeto.fintrack.handler;
 
-public record ErrorResponse() {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ErrorResponse(
+        int status,
+        String error,
+        String message,
+        LocalDateTime timestamp,
+        List<String> details
+) {
+    public ErrorResponse(int status, String error, String message) {
+        this(status, error, message, LocalDateTime.now(), null);
+    }
+
+    public ErrorResponse(int status, String error, String message, List<String> details) {
+        this(status, error, message, LocalDateTime.now(), details);
+    }
 }
